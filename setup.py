05900b30167a54c67d7e613a7e90470d6f44605e
Danielle Rothermel <drotherm@fb.com>
2021-01-12 14:56:31 -0800

    add pip setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    reqs = [line.strip() for line in f]
+
+setup(
+  name="rlstructures",
+  version="1.0",
+  python_requires=">=3.7",
+  packages=find_packages(),
+  install_requires=reqs,
+)
