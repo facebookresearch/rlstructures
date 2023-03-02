dbe46a33e0676fc2350bfeb1850832e0eab60fd6
jbgaya <jbgaya@fb.com>
2023-03-02 02:25:23 -0800

    fix headers

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
